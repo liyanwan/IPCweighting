dc476f3a72fa32265efeee200cfc4713438a0b4a
Liyan Wang <159169903+liyanwan@users.noreply.github.com>
2025-08-14 00:09:22 -0400

    Update dIPCW_ML.R

--- dIPCW_ML/R/dIPCW_ML.R
+++ dIPCW_ML/R/dIPCW_ML.R
@@ -79,6 +79,7 @@ dIPCW_ML <- function(time_point,
   if(include_SA){
     if(any(sapply(learner_list, identical, y = base_mars_BinnedIPCW))){
       add_cox_test_EP = additive_cox(train_data = train_data, test_X = newX, time_point = time_point,
+                                     cts.num = ifelse(!is.null(surv_params$cts.num), surv_params$cts.num, 5),
                                      k = ifelse(!is.null(surv_params$k), surv_params$k, 10))
     }
     if(any(sapply(learner_list, identical, y = base_glmnet_BinnedIPCW))){
